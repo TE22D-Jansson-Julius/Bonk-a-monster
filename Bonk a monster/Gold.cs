b9586307dc7ffc76b9349f3a17c3eb80a0aa6057
Julius Jansson <julius.jansson@elev.ga.ntig.se>
2024-09-03 10:13:03 +0200

    first commit

---
+++ Bonk a monster/Gold.cs
@@ -0,0 +1,7 @@
+﻿namespace Bonk_a_monster;
+
+public class GoldChomp
+{
+public string Name = "Gold Chomper";
+public int Hp = 100; 
+}
---
+++ Bonk a monster/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Chomp name = new(){
+    public string name = "Chomper";
+public int chompHp = 100; 
+};
+while()
+ active = attack;
+
+Console.WriteLine();
+Console.ReadLine();
